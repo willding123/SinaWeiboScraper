165a6e77fc0008eed2886c43e006059295a95718
willding123 <50746497+willding123@users.noreply.github.com>
2020-12-18 02:53:14 -0500

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@
 
 # Author: Xuzhou Yin 
 from bs4 import BeautifulSoup
+import struct
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,6 +12,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 import urllib
+import urllib.parse
 from selenium import webdriver
 import datetime
 import time as systime
@@ -17,7 +20,7 @@ from selenium.webdriver.firefox.webdriver import FirefoxProfile
 import unicodecsv as csv
 base_url = 'http://s.weibo.com/weibo/'
 file = "query"
-file_index = 6
+file_index = 9
 def scrap():
 	global file_index
 	with open('query.txt') as f:
@@ -28,76 +31,54 @@ def scrap():
 		query = each
 		s = each.split(';')
 		keyword = s[0]# urllib.quote(urllib.quote(s[0]))
-		date = s[1]
-		start = s[2]
-		end = s[3]
-		page = s[4]
-		scrap_each_query(keyword, date, start, end, page, query)
+# 		date = s[1]
+		start = s[1]
+		end = s[2]
+		page = s[3]
+		scrap_each_query(keyword, start, end, page, query)
 		file_index = file_index + 1
 
-def scrap_each_query(keyword, date, start, end, page, query):
+def scrap_each_query(keyword, start, end, page, query):
 	real_keyword = keyword
-	keyword = urllib.quote(urllib.quote(keyword))
-	# login_url = 'http://m.weibo.com/'
-	# driver = webdriver.Chrome()
-	# driver.get(login_url)
-	# driver.implicitly_wait(2)
-	# string = '登录'
-	# driver.find_element_by_link_text ( string.decode('utf-8') ).click()
-	# driver.implicitly_wait(2)
-	# driver.find_element_by_link_text(string.decode('utf-8') ).click()
-	# savedCookies = driver.get_cookies()
-	# # login code
-	# pickle.dump(driver.get_cookies() , open("chrome.pkl","wb"))
-	# driver.close()
+	keyword = urllib.parse.quote(urllib.parse.quote(keyword))
+	print(keyword)
 	all_content = []
 	all_time = []
-	profile = FirefoxProfile("/Users/xuzhouyin/Library/Application Support/Firefox/Profiles/ky3euswa.default-1457644506160")
+	profile = FirefoxProfile("/Users/williamding/Library/Application Support/Firefox/Profiles/6irszdj9.default-release")
 	driver = webdriver.Firefox(profile)
-	# co = webdriver.ChromeOptions()
-	# co.add_argument('user-data-dir=/Users/xuzhouyin/Library/Application Support/Google/Chrome/')
-	# driver = webdriver.Chrome(chrome_options = co)
 	url = base_url + keyword + "&typeall=1&suball=1&timescope=custom:" + start + ":" + end + "&page=" + "1"
 	driver.get(url)
 	systime.sleep(5)
 	for i in range(int(page)):
-		url = base_url + keyword + "&typeall=1&suball=1&timescope=custom:" + start + ":" + end + "&page=" + str(i + 1)
-		# url = "http://s.weibo.com/weibo/%25E5%2585%2583%25E6%2597%25A6&typeall=1&suball=1&timescope=custom:2016-12-31:2016-12-31&Refer=g"
-		# chrome_options = Options()
-		# chrome_options.add_argument("~/Library/Application Support/Google/Chrome/Default");
-		# co = webdriver.ChromeOptions()
-		# co.add_argument('/Users/xuzhouyin/Library/Application\ Support/Google/Chrome/Default')
-		
-		# for cookie in pickle.load(open("chrome.pkl", "rb")):
-		# 	driver.add_cookie(cookie)
+		url = base_url + keyword + "&typeall=1&suball=1&timescope=custom:" + start + ":" + end + "&page=" + str(i + 1)	
 		driver.get(url)
-		
-		# driver.magage().add_cookie(savedCookies)
 		page_source = driver.page_source
-		soup = BeautifulSoup(page_source, "html.parser")
-		content = soup.findAll("p", { "class" : "comment_txt" })
-		time = soup.findAll("a", { "class" : "W_textb" })
-		
+		soup = BeautifulSoup(page_source, "lxml")
+		content = soup.find_all("p", { "class" : "txt" ,"node-type":"feed_list_content"})
+		time = soup.find_all( "a", {"suda-data" :re.compile(".*wb_time.*")})
+# 		href =re.compile("//weibo.com/"), target = "_blank"
 		for each in content:
-			all_content.append(each.get_text().encode('utf-8'))
+			all_content.append(each.get_text())
 		for each in time:
-			each = each.get_text()
-			each = each.encode('utf-8')
-			time = ""
+			each.get_text()
+# 			each = each.encode('utf-8')
+			t = ""
 			if "月" in each:
-				time = str(datetime.datetime.now().year) + "-" + each[0:each.index("月")] + "-" + each[(each.index("月") + 3):each.index("日")]
-			else:
-				time = each[0:each.index(" ")]
-			all_time.append(time)
+				t = each[(each.index("月")-1):(each.index("月"))]+ each[(each.index("月") + 1):(each.index("月") + 3)] 
+# =============================================================================
+# 			else:
+# 				time = each[0:each.index()]
+# =============================================================================
+			all_time.append(t)
 	driver.close()
-	save_to_csv(file + str(file_index), real_keyword, date, all_content, all_time, query)
+	save_to_csv(file + str(file_index), real_keyword, all_content, all_time, query)
 
-def save_to_csv(filename, keyword, date, content, time, query):
-	with open('./output/' + filename + '.csv', 'w') as csvfile:
-	    spamwriter = csv.writer(csvfile, dialect='excel', encoding='utf-16')
-	    spamwriter.writerow(["query", "Post ID", "keyword", "event Date", "Post Content", "Post Time"])
-	    for i in range(len(content)):
-	    	spamwriter.writerow([query, i + 1, keyword, date, content[i], time[i]])
+def save_to_csv(filename, keyword, content, time, query):
+	with open('./output/' + filename + '.csv', 'wb') as csvfile:
+		spamwriter = csv.writer(csvfile, dialect='excel', encoding='utf-8')
+		spamwriter.writerow(["query", "Post ID", "keyword", "Post Time", "Post Content" ])
+		for i in range(len(content)):
+			spamwriter.writerow([query, i +1, keyword, time[i],content[i]])
 
 scrap()
 
